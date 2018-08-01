7aa692db35e1cecaba98a976fd347d3d7a243377
Dan Blanchard <dan.blanchard@gmail.com>
2018-08-01 13:43:35 +0200

    Fix docker login in deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 docker tag $REPO:${TRAVIS_COMMIT} $REPO:${TAG}
 docker tag $REPO:${TRAVIS_COMMIT} $REPO:travis-$TRAVIS_BUILD_NUMBER
